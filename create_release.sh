a6bd380512e43105bd6fcda84212aa5efe536e7c
Andrew Stromme <andrew.stromme@gmail.com>
2014-09-06 11:59:50 -0700

    improved release script

--- create_release.sh
+++ create_release.sh
@@ -5,6 +5,11 @@ cd "$(dirname $0)"
 version=$1
 filename="releases/Edge-${version}.zip"
 
+if [ -z "${version}" ]; then
+    echo "Usage: ${0} version_number"
+    exit 1
+fi
+
 echo "stashing changes since last commit"
 git stash save "create_release.sh"
 
@@ -13,6 +18,10 @@ git tag "${version}"
 
 echo "zipping into ${filename}"
 zip -r "${filename}" app
+
+echo "adding release zip to git"
+git add "${filename"}
+git commit -m "adding version ${version}"
 
 echo "restoring changes since last commit"
 git stash pop
